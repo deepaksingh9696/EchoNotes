683dbfbae6de91123f77c12a9676bee92e355443
deepaksingh9696 <deepakaingh8881gkp@gmail.com>
2024-08-04 12:46:36 +0530

    upload file

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const authRoutes = require("./Routes/auth");
+const noteRoutes = require("./Routes/notes");
+
+const app = express();
+const PORT = 7070;
+
+dotenv.config();
+app.use(cors());
+app.use(bodyParser.json());
+
+try {
+    mongoose.connect(process.env.MONGO_URL);
+    console.log("Connection Successfull");
+} catch (error) {
+    console.log(error);
+}
+
+app.get("/", (req, res) => {
+    res.send("Server Is Running");
+});
+
+
+app.use("/auth", authRoutes);
+app.use("/notes", noteRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server Running on Port ${PORT}`);
+})
+
+
+
